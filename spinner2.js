2b33dcf43b023600134af66ba401fc1364ffd8ec
Hayder-Suwaed <haiderhachim87@yahoo.com>
2019-10-22 19:49:17 +0000

    done

---
+++ spinner2.js
@@ -0,0 +1,50 @@
+var delay = 0;
+var spinner = "|/-\\|/-\\|\n"
+
+ for (const char of spinner) {
+  setTimeout (() => {
+   process.stdout.write('\r'  + char +  '')
+   }, delay ); 
+  delay += 200;
+ }
+setTimeout(() => {
+
+process.stdout.write('\n');
+}),
+delay += 200;
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+//  setTimeout(() => {
+//   process.stdout.write('\r|   '); 
+// }, 900);
+
+//  setTimeout(() => {
+//   process.stdout.write('\r-   '); 
+// }, 1100);
+
+//  setTimeout(() => {
+//   process.stdout.write('\r\\   '); 
+// }, 1300);
+
+//  setTimeout(() => {
+//   process.stdout.write('\r|   '); 
+// }, 1500);
+
+// setTimeout(() => {
+//   process.stdout.write('\n  '); 
+// }, 1100);
