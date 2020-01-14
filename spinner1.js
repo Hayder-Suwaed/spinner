929d8647aecf12e721d2178ad3209fbd39c2dbea
Hayder-Suwaed <haiderhachim87@yahoo.com>
2020-01-14 20:59:45 +0000

    Spinner with Thaigo and Qusai

--- spinner1.js
+++ spinner1.js
@@ -1,35 +1,12 @@
-setTimeout(() => {
-  process.stdout.write('\r|   ');
-}, 100);
-
-setTimeout(() => {
-  process.stdout.write('\r/   ');
-}, 300);
-
-setTimeout(() => {
-  process.stdout.write('\r-   ');
-}, 500);
-
-setTimeout(() => {
-  // Need to escape the backslash since it's a special character.
-  process.stdout.write('\r\\   '); 
-}, 700);
- setTimeout(() => {
-  process.stdout.write('\r|   '); 
-}, 900);
-
- setTimeout(() => {
-  process.stdout.write('\r-   '); 
-}, 1100);
-
- setTimeout(() => {
-  process.stdout.write('\r\\   '); 
-}, 1300);
-
- setTimeout(() => {
-  process.stdout.write('\r|   '); 
-}, 1500);
-
-setTimeout(() => {
-  process.stdout.write('\n  '); 
-}, 1100);
+process.stdout.write('hello from spinner1.js... \rheyyy\n');
+const vTxt = "|/-\|/-\\|"
+let delay = 50;
+for (const char of vTxt) {
+  setTimeout(() => {
+    process.stdout.write("\r" + char);
+  }, delay);
+  delay += 200;
+}
+setTimeout (() =>{
+  process.stdout.write('\n')
+}, delay += 200);
--- spinner2.js
+++ spinner2.js
@@ -1,50 +1,13 @@
 var delay = 0;
-var spinner = "|/-\\|/-\\|\n"
+var spinner = "|/-\\|/-\\|\n";
 
- for (const char of spinner) {
-  setTimeout (() => {
-   process.stdout.write('\r'  + char +  '')
-   }, delay ); 
+for (const char of spinner) {
+  setTimeout(() => {
+    process.stdout.write("\r" + char + "");
+  }, delay);
   delay += 200;
- }
+}
 setTimeout(() => {
-
-process.stdout.write('\n');
+  process.stdout.write("\n");
 }),
-delay += 200;
-
-// setTimeout(() => {
-//   process.stdout.write('\r|   ');
-// }, 100);
-
-// setTimeout(() => {
-//   process.stdout.write('\r/   ');
-// }, 300);
-
-// setTimeout(() => {
-//   process.stdout.write('\r-   ');
-// }, 500);
-
-// setTimeout(() => {
-//   // Need to escape the backslash since it's a special character.
-//   process.stdout.write('\r\\   '); 
-// }, 700);
-//  setTimeout(() => {
-//   process.stdout.write('\r|   '); 
-// }, 900);
-
-//  setTimeout(() => {
-//   process.stdout.write('\r-   '); 
-// }, 1100);
-
-//  setTimeout(() => {
-//   process.stdout.write('\r\\   '); 
-// }, 1300);
-
-//  setTimeout(() => {
-//   process.stdout.write('\r|   '); 
-// }, 1500);
-
-// setTimeout(() => {
-//   process.stdout.write('\n  '); 
-// }, 1100);
+  (delay += 200);
